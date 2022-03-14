6b7e0a8c1ee8f69dffbca45fda36c6bf70628ad6
Fortesa-Kraser <84673573+Fortesa-Kraser@users.noreply.github.com>
2022-03-14 23:57:49 +0100

    Add files via upload

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <div v-if="user.data">Hello {{ user.data.email }}</div>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import { mapGetters } from "vuex";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+  computed: {
+    ...mapGetters({
+      user: "user",
+    }),
+  },
+};
+</script>
