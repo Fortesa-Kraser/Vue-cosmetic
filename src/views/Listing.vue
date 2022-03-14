af8c1a9a29f069732c793c8a83d2d14a5ef01a32
Diellza-Aliuu <84675457+Diellza-Aliuu@users.noreply.github.com>
2022-03-15 00:16:24 +0100

    Add files via upload

---
+++ src/views/Listing.vue
@@ -0,0 +1,42 @@
+<template>
+  <section>
+    <div class="container">
+      <div class="row">
+        <Card
+          v-for="entry in vueCosmeticList"
+          :key="entry._id"
+          :vueCosmetic="entry"
+        />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Card from "@/components/Card.vue";
+import apiRequest from "../utility/apiRequest";
+import { mapGetters } from "vuex";
+export default {
+  components: {
+    Card,
+  },
+  created() {
+    this.fetchVueCosmetic();
+  },
+  methods: {
+    async fetchvueCosmetic() {
+      const result = await apiRequest.getVueCosmeticList();
+      this.$store.dispatch("fetchvueCosmetics", result);
+    },
+  },
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapGetters({
+      user: "user",
+      vueCosmeticList: "vueCosmeticsList",
+    }),
+  },
+};
+</script>
