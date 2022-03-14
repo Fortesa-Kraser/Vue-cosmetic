ada0f6f357373ca96f4cf3f709c880725d802a9c
Fortesa-Kraser <84673573+Fortesa-Kraser@users.noreply.github.com>
2022-03-14 23:16:13 +0100

    Create NumberInput.vue

---
+++ src/components/form/NumberInput.vue
@@ -0,0 +1,16 @@
+<template>
+  <input
+    class="form-control"
+    type="number"
+    :value="value"
+    @input="$emit('input', $event.target.value)"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    value: Number,
+  },
+};
+</script>
