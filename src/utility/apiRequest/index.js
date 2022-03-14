0adc140b5cc1f91de567ad299a92e452c77cbd4f
Aroma-Plakolli <79229323+Aroma-Plakolli@users.noreply.github.com>
2022-03-14 23:36:34 +0100

    Create index.js

---
+++ src/utility/apiRequest/index.js
@@ -0,0 +1,17 @@
+import registerUser from "./user/registerUser";
+import getvueCosmeticList from "./vueCosmetic/getvueCosmetic";
+import createvueCosmetic from "./vueCosmetic/createvueCosmetic";
+import getRvueCosmetic from "./vueCosmetic/getvueCosmetic";
+import getFilesDetails from "./vueCosmetic/getvueCosmetic";
+import removeFile from "./vueCosmetic/removeFile";
+import deletevueCosmetic from "./vueCosmetic/deletevueCosmetic";
+
+export default {
+  registerUser,
+  getvueCosmeticList,
+  getvueCosmetic,
+  createvueCosmetic,
+  getFilesDetails,
+  removeFile,
+  deletevueCosmetic,
+};
