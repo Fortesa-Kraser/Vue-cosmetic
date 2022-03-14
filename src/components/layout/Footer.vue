4f8f2613d602e7d94bccf21d38bb3d5341a2647d
Aroma-Plakolli <79229323+Aroma-Plakolli@users.noreply.github.com>
2022-03-14 23:08:01 +0100

    Create Footer.vue

---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer class="text-center bg-light text-muted fixed-bottom">
+    <div class="p-4">© {{ currentYear }} Copyright: Vue Cosmetic App</div>
+  </footer>
+</template>
+
+<script>
+export default {
+  computed: {
+    currentYear() {
+      return new Date().getFullYear();
+    },
+  },
+};
+</script>
+
+<style>
+body {
+  padding-bottom: 80px;
+}
+</style>
