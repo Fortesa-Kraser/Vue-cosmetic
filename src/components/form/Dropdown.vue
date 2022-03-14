816eb02813566c30e5bba4ab89c19e932029736f
Aroma-Plakolli <79229323+Aroma-Plakolli@users.noreply.github.com>
2022-03-14 23:25:35 +0100

    Create Dropdown.vue

---
+++ src/components/form/Dropdown.vue
@@ -0,0 +1,20 @@
+<template>
+  <select
+    class="form-control"
+    :value="value"
+    @input="$emit('input', $event.target.value)"
+  >
+    <option v-for="entry in options" :key="entry.value" :value="entry.value">
+      {{ entry.label }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  props: {
+    value: String,
+    options: Array, // { label: 'Apartment', value: 2 }
+  },
+};
+</script>
