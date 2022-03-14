8414b09b168c55893072cf00e20e958c3040af64
Aroma-Plakolli <79229323+Aroma-Plakolli@users.noreply.github.com>
2022-03-14 22:40:19 +0100

    Create getAuthToken.js

---
+++ vue-cosmetic-api/api/middlewares/getAuthToken.js
@@ -0,0 +1,12 @@
+const getAuthToken  = (req, res, next) => {
+    const authorizationHeader = req.headers.authorization;
+    if(authorizationHeader && authorizationHeader.split(' ')[0] === 'Bearer'){
+      req.authToken = authorizationHeader.split(' ')[1]
+    }
+    else{
+      req.authToken = null;
+    }
+    next();
+}
+
+export default getAuthToken;
