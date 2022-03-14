38f4a11deaab2e899cd057dfe8da6e82a897a170
Fortesa-Kraser <84673573+Fortesa-Kraser@users.noreply.github.com>
2022-03-14 23:18:02 +0100

    Create Crad.vue

---
+++ src/components/Crad.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="col-lg-4 mt-4 col-md-6 col-sm-12">
+    <div class="card">
+      <img
+        v-if="src"
+        :src="src"
+        class="card-img-top"
+        style="max-height: 150px"
+      />
+      <div class="card-body">
+        <h5 class="card-title">{{ vueCosmetic.type }}</h5>
+        <p class="card-text">Square meters: {{ vueCosmetic.squareMeters }}</p>
+        <router-link
+          class="btn btn-primary"
+          :to="{ name: 'View', params: { id: vueCosmetic._id } }"
+          >View</router-link
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    vueCosmetic: Object,
+  },
+  computed: {
+    src() {
+      const filename = this.vueCosmetic.files?.split(";")[0];
+
+      return filename ? `http://localhost:4000/static/${filename}` : null;
+    },
+  },
+};
+</script>
